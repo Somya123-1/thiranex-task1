cb8b47de574a8a8c787931db854b1ac9158aca09
Somya123-1 <peetusingh858@gmail.com>
2026-07-03 16:08:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+function showMessage() {
+    alert("Hello from Thiranex Task 1! Welcome to my website.");
+}
+
+// Page load hone pe message
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Website loaded successfully!");
+});
